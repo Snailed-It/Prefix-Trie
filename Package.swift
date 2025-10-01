bd809c94a57d8da9d8079e667bc1b631e7886e0d
David Stephens <git@dwrs.org>
2025-10-01 21:12:41 +0100

    Remove unused dependencies from when this was embedded in a larger tool

--- Package.swift
+++ Package.swift
@@ -14,8 +14,6 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-collections", from: "1.3.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.6.1"),
-        .package(url: "https://github.com/hirotakan/MessagePacker", from: "0.4.7"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -24,11 +22,7 @@ let package = Package(
             name: "PrefixTrie",
             dependencies: [
                 .product(name: "OrderedCollections",
-                    package: "swift-collections"),
-                .product(name: "ArgumentParser",
-                    package: "swift-argument-parser"),
-                .product(name: "MessagePacker",
-                    package: "MessagePacker")
+                    package: "swift-collections")
             ]
         ),
         .testTarget(
